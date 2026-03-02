ec57154d7e76154a082ca24d157141c4c8673ad3
Maria ClaraCortez <mariac.dias@edu.senai.br>
2026-03-02 17:11:41 -0300

    ADD: Criado o jogo mad libs

---
+++ 01_madf_libs/Historinha1.py
@@ -0,0 +1,46 @@
+print('''
+      
+       _       __    ___   _     _   ___   
+| |\/|  / /\  | | \ | |   | | | |_) ( (` 
+|_|  | /_/--\ |_|_/ |_|__ |_| |_|_) _)_) 
+      
+  _   _  _  ___   _____  _____  ___    _  _   _  _   _  _____            _____  _      _   _  ___    _____ 
+( ) ( )(_)(  _`\(_   _)(  _  )|  _`\ (_)( ) ( )( ) ( )(  _  )   /'\_/`\(  _  )( )    ( ) ( )(  _`\ (  _  )
+| |_| || || (_(_) | |  | ( ) || (_) )| || `\| || |_| || (_) |   |     || (_) || |    | | | || ( (_)| (_) |
+|  _  || |`\__ \  | |  | | | || ,  / | || , ` ||  _  ||  _  |   | (_) ||  _  || |  _ | | | || |  _ |  _  |
+| | | || |( )_) | | |  | (_) || |\ \ | || |`\ || | | || | | |   | | | || | | || |_( )| (_) || (_( )| | | |
+(_) (_)(_)`\____) (_)  (_____)(_) (_)(_)(_) (_)(_) (_)(_) (_)   (_) (_)(_) (_)(____/'(_____)(____/'(_) (_)
+                                                                                                          
+                                                                                                                                                                                                                                                                                                                        
+    ''')
+
+verbo=input("Qual o verbo?")
+verbo2=input("Qual o segundo verbo?")
+verbo3=input("Qual o terceiro verbo?")
+verbo4=input("Qual o quarto verbo?")
+
+arma= input('Qual a arma?')
+
+animal=input("Qual o animal?")
+
+adjetivo=input("Qual o adjetivo?")
+adjetivo2=input("Qual o segundo adjetivo?")
+
+lugar=input("Qual o lugar?")
+lugar2=input("Qual o segundo lugar?")
+
+famoso= input("Qual o famoso?")
+
+emoção=input("Qual a emoção?")
+emoção2=input("Qual a segunda emoção?")
+emoção3=input("Qual a terceira emoção?")
+
+print(f'''Era uma vez um {adjetivo} {animal} que acordou no {lugar} com uma {arma} {adjetivo2} e decidiu {verbo}, achando que seria o herói mais épico de todos os tempos.
+
+Enquanto atravessava o {lugar2}, ele começou a {verbo2} de {emoção} porque precisava cagar, mas todos os cantos estavam ocupados por {animal} fazendo dancinhas de TikTok e outros {animal} derrubando objetos aleatórios como se fosse um meme.
+
+De repente, o {famoso} apareceu e começou a verbo3, espalhando baldes de lama, rolos de papel higiênico e até carrinhos de algodão-doce, transformando o chão em uma pista de escorregões digna de viral.
+
+O {animal}, agora desesperado, {verbo4}, escorregando e rolando pelo chão, espalhando cocô, lama e glitter por todo o {lugar2}, causando {emoção2} e {emoção3}: todos os {animal} rolando de rir, alguns vomitando de tanto rir, e o {animal} parecendo um meme ambulante.
+
+No final, o {animal} aprendeu que aventuras podem ser engraçadas, mas cagar no meio de uma catástrofe total é definitivamente nível lendário de meme.''')

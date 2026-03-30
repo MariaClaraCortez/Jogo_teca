1dd1483a710f04ef4ad1eb5c0e581af673ea41ec
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-03-30 14:28:29 -0300

    ADD: fiz um jogo de impar ou par

---
+++ 04_par_ou_impar/par_ou_impar.py
@@ -0,0 +1,41 @@
+import random
+print("""
+                                                                                    
+    |                   ,---.                            |                    
+    |,---.,---.,---.    |---',---.,---.    ,---..   .    |,-.-.,---.,---.,---.
+    ||   ||   ||   |    |    ,---||        |   ||   |    || | ||   |,---||    
+`---'`---'`---|`---'    `    `---^`        `---'`---'    `` ' '|---'`---^`    
+          `---'                                                |              
+
+      """)
+
+lado = input("Impar ou Par?").upper()
+if lado == "IMPAR":
+    print("Então eu sou o par")
+else:
+    print("Então eu sou o impar")
+
+
+
+numero1 = int(input("Qual será o número de 0 a 10 escolhido?"))
+numero2 = random.randint(0,10)
+
+if numero1 >10 or numero1 <0:
+    print("Você nâo tem esse tanto de dedos")
+else:
+    if lado == 'IMPAR':
+        resultado= numero1 + numero2
+        print(f"{numero1} + {numero2}")
+        if resultado % 2 == 1:
+            print(f"Parebens vc ganhou, o {resultado} é impar")
+        else:
+            print(f"Você perdeu, o {resultado} é par")
+
+    if lado == 'PAR':
+        resultado= numero1 + numero2
+        print(f"{numero1} + {numero2}")
+        if resultado % 2 == 0:
+            print(f"Parebens vc ganhou, o {resultado} é par")
+        else:
+            print(f"Você perdeu, o {resultado} é impar")
+

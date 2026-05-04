43450716a7449398c12c2a9f9e8277e6ada9a0ab
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-05-04 14:58:36 -0300

    ADD menu + sair

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from j01_madf_libs import jogar_historinha
-from j02_adivinha_numero import jogar_adivinha_numero
-from j03_tabuada import jogar_tabuada
-from j04_par_ou_impar import jogar_par_ou_impar
-from j05_qual_vc_escolhe import jogar_caos
-from j06_cara_coroa import jogar_cara_ou_coroa
+
+from j01_madf_libs.Historinha1 import jogar_historinha
+from j02_adivinha_numero.adivinha_numero import jogar_adivinha_numero
+from j03_tabuada.jogo_da_tabuada import jogar_tabuada
+from j04_par_ou_impar.par_ou_impar import jogar_par_ou_impar
+from j05_qual_vc_escolhe.caos import jogar_caos
+from j06_cara_coroa.cara_ou_coroa import jogar_cara_ou_coroa
 
 print("""
           )             )                               
@@ -16,41 +17,45 @@ print("""
  \__/  \___/   \___| \___/    |_|  |___|  \___|/_/ \_\  
                                                         
 """)
-
-print("""
-***********************************************************************
-┏━┓╺┓          ╻ ╻╻┏━┓╺┳╸┏━┓┏━┓╻┏┓╻╻ ╻┏━┓                         
-┃┃┃ ┃    ╺━╸   ┣━┫┃┗━┓ ┃ ┃ ┃┣┳┛┃┃┗┫┣━┫┣━┫                         
-┗━┛╺┻╸         ╹ ╹╹┗━┛ ╹ ┗━┛╹┗╸╹╹ ╹╹ ╹╹ ╹                         
-┏━┓┏━┓         ┏━┓╺┳┓╻╻ ╻╻┏┓╻╻ ╻┏━┓   ┏━┓   ┏┓╻╻ ╻┏┳┓┏━╸┏━┓┏━┓    
-┃┃┃┏━┛   ╺━╸   ┣━┫ ┃┃┃┃┏┛┃┃┗┫┣━┫┣━┫   ┃ ┃   ┃┗┫┃ ┃┃┃┃┣╸ ┣┳┛┃ ┃    
-┗━┛┗━╸         ╹ ╹╺┻┛╹┗┛ ╹╹ ╹╹ ╹╹ ╹   ┗━┛   ╹ ╹┗━┛╹ ╹┗━╸╹┗╸┗━┛    
-┏━┓┏━┓         ╺┳╸┏━┓┏┓ ╻ ╻┏━┓╺┳┓┏━┓                              
-┃┃┃╺━┫   ╺━╸    ┃ ┣━┫┣┻┓┃ ┃┣━┫ ┃┃┣━┫                              
-┗━┛┗━┛          ╹ ╹ ╹┗━┛┗━┛╹ ╹╺┻┛╹ ╹                              
-┏━┓╻ ╻         ┏━┓┏━┓┏━┓   ┏━┓╻ ╻   ╻┏┳┓┏━┓┏━┓┏━┓                 
-┃┃┃┗━┫   ╺━╸   ┣━┛┣━┫┣┳┛   ┃ ┃┃ ┃   ┃┃┃┃┣━┛┣━┫┣┳┛                 
-┗━┛  ╹         ╹  ╹ ╹╹┗╸   ┗━┛┗━┛   ╹╹ ╹╹  ╹ ╹╹┗╸                 
-┏━┓┏━╸         ┏━╸┏━┓┏━┓┏━┓
-┃┃┃┗━┓   ╺━╸   ┃  ┣━┫┃ ┃┗━┓
-┗━┛┗━┛         ┗━╸╹ ╹┗━┛┗━┛
-┏━┓┏━┓         ┏━╸┏━┓┏━┓┏━┓   ┏━┓╻ ╻   ┏━╸┏━┓┏━┓┏━┓┏━┓            
-┃┃┃┣━┓   ╺━╸   ┃  ┣━┫┣┳┛┣━┫   ┃ ┃┃ ┃   ┃  ┃ ┃┣┳┛┃ ┃┣━┫            
-┗━┛┗━┛         ┗━╸╹ ╹╹┗╸╹ ╹   ┗━┛┗━┛   ┗━╸┗━┛╹┗╸┗━┛╹ ╹            
-***********************************************************************
-""")
-jogo = int(input("Qual jogo você quer jogar???"))
-
-
-if jogo == 1:
-    jogar_historinha()
-elif jogo == 2:
-    jogar_adivinha_numero()
-elif jogo == 3:
-    jogar_tabuada()
-elif jogo == 4:
-    jogar_par_ou_impar()
-elif jogo == 5:
-    jogar_caos()
-elif jogo == 6:
-    jogar_cara_ou_coroa()
+while True:
+    print("""
+    ***********************************************************************
+    ┏━┓┏━┓         ┏━┓┏━┓╻┏━┓
+    ┃┃┃┃┃┃   ╺━╸   ┗━┓┣━┫┃┣┳┛
+    ┗━┛┗━┛         ┗━┛╹ ╹╹╹┗╸
+    ┏━┓╺┓          ╻ ╻╻┏━┓╺┳╸┏━┓┏━┓╻┏┓╻╻ ╻┏━┓                         
+    ┃┃┃ ┃    ╺━╸   ┣━┫┃┗━┓ ┃ ┃ ┃┣┳┛┃┃┗┫┣━┫┣━┫                         
+    ┗━┛╺┻╸         ╹ ╹╹┗━┛ ╹ ┗━┛╹┗╸╹╹ ╹╹ ╹╹ ╹                         
+    ┏━┓┏━┓         ┏━┓╺┳┓╻╻ ╻╻┏┓╻╻ ╻┏━┓   ┏━┓   ┏┓╻╻ ╻┏┳┓┏━╸┏━┓┏━┓    
+    ┃┃┃┏━┛   ╺━╸   ┣━┫ ┃┃┃┃┏┛┃┃┗┫┣━┫┣━┫   ┃ ┃   ┃┗┫┃ ┃┃┃┃┣╸ ┣┳┛┃ ┃    
+    ┗━┛┗━╸         ╹ ╹╺┻┛╹┗┛ ╹╹ ╹╹ ╹╹ ╹   ┗━┛   ╹ ╹┗━┛╹ ╹┗━╸╹┗╸┗━┛    
+    ┏━┓┏━┓         ╺┳╸┏━┓┏┓ ╻ ╻┏━┓╺┳┓┏━┓                              
+    ┃┃┃╺━┫   ╺━╸    ┃ ┣━┫┣┻┓┃ ┃┣━┫ ┃┃┣━┫                              
+    ┗━┛┗━┛          ╹ ╹ ╹┗━┛┗━┛╹ ╹╺┻┛╹ ╹                              
+    ┏━┓╻ ╻         ┏━┓┏━┓┏━┓   ┏━┓╻ ╻   ╻┏┳┓┏━┓┏━┓┏━┓                 
+    ┃┃┃┗━┫   ╺━╸   ┣━┛┣━┫┣┳┛   ┃ ┃┃ ┃   ┃┃┃┃┣━┛┣━┫┣┳┛                 
+    ┗━┛  ╹         ╹  ╹ ╹╹┗╸   ┗━┛┗━┛   ╹╹ ╹╹  ╹ ╹╹┗╸                 
+    ┏━┓┏━╸         ┏━╸┏━┓┏━┓┏━┓
+    ┃┃┃┗━┓   ╺━╸   ┃  ┣━┫┃ ┃┗━┓
+    ┗━┛┗━┛         ┗━╸╹ ╹┗━┛┗━┛
+    ┏━┓┏━┓         ┏━╸┏━┓┏━┓┏━┓   ┏━┓╻ ╻   ┏━╸┏━┓┏━┓┏━┓┏━┓            
+    ┃┃┃┣━┓   ╺━╸   ┃  ┣━┫┣┳┛┣━┫   ┃ ┃┃ ┃   ┃  ┃ ┃┣┳┛┃ ┃┣━┫            
+    ┗━┛┗━┛         ┗━╸╹ ╹╹┗╸╹ ╹   ┗━┛┗━┛   ┗━╸┗━┛╹┗╸┗━┛╹ ╹            
+    ***********************************************************************
+    """)
+    jogo = int(input("Qual jogo você quer jogar???"))
+    if jogo == 1:
+        jogar_historinha()
+    elif jogo == 2:
+        jogar_adivinha_numero()
+    elif jogo == 3:
+        jogar_tabuada()
+    elif jogo == 4:
+        jogar_par_ou_impar()
+    elif jogo == 5:
+        jogar_caos()
+    elif jogo == 6:
+        jogar_cara_ou_coroa()
+    elif jogo == 0:
+        print("Foi ótimo jogar com você")
+        break
